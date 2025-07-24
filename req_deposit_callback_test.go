113cf54dfc13cd6e82338e67787ab420e8776435
cyrildou <cyrildou@live.cn>
2025-07-25 03:20:49 +0800

    打开sign验证

--- req_deposit_callback_test.go
+++ req_deposit_callback_test.go
@@ -31,15 +31,17 @@ func GenDepositCallbackRequestDemo() TCPayCreatePaymentBackReq {
 		Description: "Success",
 		Data: &TCPayCreatePaymentBackReqData{
 			Action:         "50",
-			AdditionalData: "25f5bed15931dbe97776edf4211f263a",
-			Amount:         "24.00",
-			InvoiceNumber:  "202507211626030579",
+			AdditionalData: "bc1a0e9f324c34db4b88f3d8027a1e08",
+			Amount:         "201.00",
+			InvoiceNumber:  "202507242147030906",
 			MerchantId:     "200157",
 			TerminalId:     "300384",
-			Token:          "",
+			Token:          "YmC4cfhd2oCRlqXr3Upfd2qgpD0aqepzSimCIh_FJFA",
 		}, //不能浮点数
 	}
 }
+
+//Origin=Website&resCode=0&description=Success&data.Action=50&data.AdditionalData=bc1a0e9f324c34db4b88f3d8027a1e08&data.Amount=201.00&data.InvoiceNumber=202507242147030906&data.MerchantId=200157&data.TerminalId=300384&data.Token=YmC4cfhd2oCRlqXr3Upfd2qgpD0aqepzSimCIh_FJFA
 
 // Origin=Website&resCode=418&description=The+time+allowed+for+payment+has+expired.&data=
 func GenDepositCallbackRequestDemo2() TCPayCreatePaymentBackReq {
